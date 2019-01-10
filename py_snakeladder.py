8fccf8ead2695d6fdcc98566e1598ec7a743c515
Himank Pathak <himankpathak@gmail.com>
2019-01-10 23:31:58 +0530

    Initial commit

---
+++ py_snakeladder.py
@@ -0,0 +1,12 @@
+
+if __name__ == '__main__':
+    print("""
+    Welcome to Snake&Ladders v0.9
+
+    Instructions:
+    1. Everybody plays with the initials of their names
+    2. Your dice is rolled automatically when it's your turn
+
+    """)
+
+    
